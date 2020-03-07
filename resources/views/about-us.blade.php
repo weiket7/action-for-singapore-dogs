67af3e71238d5bde14d72c9aee21c8d94aabd348
WeiKet <wei_ket@hotmail.com>
2020-03-07 13:07:58 +0800

    About Us add video

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -8,7 +8,13 @@
   
     <iframe width="560" height="315" src="https://www.youtube.com/embed/8e2qHk043zE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <br><br>
-    
+
     {!! nl2br($content) !!}
+
+    <video width="600" height="400" controls>
+      <source src="ASD - A Decade of Love, Hope & Dogs.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+    <br><small>Source: https://vimeo.com/61993034</small>
   </div>
 @endsection
